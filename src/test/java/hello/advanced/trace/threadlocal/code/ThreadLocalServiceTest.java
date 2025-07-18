d12ca2ec6bcd4c804a330217f8bd344298e09c54
dfdf0202 <dfdf1245@daum.net>
2025-07-18 16:23:27 +0900

    init

---
+++ src/test/java/hello/advanced/trace/threadlocal/code/ThreadLocalServiceTest.java
@@ -0,0 +1,44 @@
+package hello.advanced.trace.threadlocal.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class ThreadLocalServiceTest {
+    private ThreadLocalService threadLocalService = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+
+        Runnable userA = () -> {
+            threadLocalService.login("userA");
+        };
+
+        Runnable userB = () -> {
+            threadLocalService.login("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("threadA");
+        Thread threadB = new Thread(userB);
+        threadB.setName("threadB");
+
+        threadA.start();
+        sleep(100);
+        threadB.start();
+
+        sleep(3000);
+        log.info("main exit");
+    }
+
+    private void sleep(int i) {
+        try {
+            Thread.sleep(i);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
